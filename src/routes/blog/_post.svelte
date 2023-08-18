31c9d7f09128e9b50f768797abdd3278ed5472fa
profmancusoa <profmancusoa@gmail.com>
2023-08-18 19:02:52 +0200

    fixing sharethis issues

--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -2,7 +2,7 @@
 	import { date_convert, cover_assign } from '$lib/helpers/helpers';
 	import Categoria from '$lib/components/Categoria.svelte';
 	import { page } from '$app/stores';
-	import { onMount } from 'svelte';
+    import { onMount } from 'svelte';
 	import { title_to_slug } from '$lib/helpers/helpers';
 	import { dev } from '$app/env';
 
@@ -82,6 +82,10 @@
 	<meta property="twitter:domain" content="prof.mancusoa.it" />
 	<meta property="twitter:url" content="{post_url}" />
 	<meta name="twitter:image" content="{base_post_url}{cover_assign(cover)}" /> 
+
+    {#key $page.path}
+        <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=64df98e5d8173e00123bb30f&product=sticky-share-buttons' async='async'></script>
+    {/key}    
 </svelte:head>
 
 <div class="sharethis-sticky-share-buttons"></div>
